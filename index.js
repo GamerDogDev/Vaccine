7f74a383695c7dfb342ebed447e55eb58b4ec6eb
!  Red <81472645+GamerDogDev@users.noreply.github.com>
2022-07-21 11:52:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 module.exports = require('./core.asar')
+
+/*
+
+
+
+░█████╗░██╗░░░██╗██████╗░░██████╗███████╗██████╗░  ██╗░░░██╗░█████╗░░█████╗░░█████╗░██╗███╗░░██╗███████╗
+██╔══██╗██║░░░██║██╔══██╗██╔════╝██╔════╝██╔══██╗  ██║░░░██║██╔══██╗██╔══██╗██╔══██╗██║████╗░██║██╔════╝
+██║░░╚═╝██║░░░██║██████╔╝╚█████╗░█████╗░░██║░░██║  ╚██╗░██╔╝███████║██║░░╚═╝██║░░╚═╝██║██╔██╗██║█████╗░░
+██║░░██╗██║░░░██║██╔══██╗░╚═══██╗██╔══╝░░██║░░██║  ░╚████╔╝░██╔══██║██║░░██╗██║░░██╗██║██║╚████║██╔══╝░░
+╚█████╔╝╚██████╔╝██║░░██║██████╔╝███████╗██████╔╝  ░░╚██╔╝░░██║░░██║╚█████╔╝╚█████╔╝██║██║░╚███║███████╗
+░╚════╝░░╚═════╝░╚═╝░░╚═╝╚═════╝░╚══════╝╚═════╝░  ░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝╚═╝░░╚══╝╚══════╝
+	
+
+
+	Desarrollado por: Rojas#2250
+
+	Fecha de creación: 20/07/2022
+
+	Versión: 1.0.0
+
+	Nota: Esta vacuna fue creada para eliminar el Inject principalmente del "DeltaStealer"
+	Aúnque también puede desinfectar cualquier otro virus que ataque el "index.js" de Discord
+	
+	Proximos cambios: Modificación y desinfección del "core.asar" donde mayormente algunos stealers atacan
+	y para personas principiantes no le es posible desinfectarlo. 
+
+
+*/
